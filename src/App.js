6b2202eb7803042ef7ef21282a01a0218b7aa2d1
Lucas O'Donnell <lucas.odonnelll@gmail.com>
2022-06-09 12:13:25 -0700

    dashboard skeleton created with sidebar and navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Landing, Register, Dashboard, Error } from "./pages";
+import { Landing, Register, Error, ProtectedRoute } from "./pages";
+import {
+  Profile,
+  AddJob,
+  AllJobs,
+  Stats,
+  SharedLayout,
+} from "./pages/dashboard";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -8,7 +15,19 @@ function App() {
     <Router>
       <ToastContainer position="top-center" />
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <SharedLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Stats />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="/landing" element={<Landing />} />
         <Route path="/Register" element={<Register />} />
         <Route path="*" element={<Error />} />
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import Logo from "./Logo";
 import FormRow from "./FormRow";
+import NavBar from "./Navbar";
+import SmallSidebar from "./SmallSidebar";
+import BigSidebar from "./BigSidebar";
 
-export { Logo, FormRow };
+export { Logo, FormRow, NavBar, SmallSidebar, BigSidebar };
