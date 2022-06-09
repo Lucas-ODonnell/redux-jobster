07c91e3b69571f1799b18dc37b0729b4435a52ee
Lucas O'Donnell <lucas.odonnelll@gmail.com>
2022-06-08 17:50:37 -0700

    registration/login functionality complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Landing, Register, Dashboard, Error } from "./pages";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <Router>
+      <ToastContainer position="top-center" />
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/landing" element={<Landing />} />
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Logo, FormRow } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { loginUser, registerUser } from "../features/user/userSlice";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 const Register = () => {
-  return <div>Register page</div>;
+  const navigate = useNavigate();
+  const [values, setValues] = useState(initialState);
+  const { user, isLoading } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!name && !isMember)) {
+      toast.error("Please Fill Out All Fields");
+      return;
+    }
+    if (isMember) {
+      dispatch(loginUser({ email, password }));
+      return;
+    }
+    dispatch(registerUser({ name, email, password }));
+  };
+
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, [navigate, user]);
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={handleSubmit}>
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {!values.isMember && (
+          <FormRow
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+            labelText="name"
+            type="text"
+          />
+        )}
+        <FormRow
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+          labelText="email"
+          type="email"
+        />
+        <FormRow
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+          labelText="password"
+          type="password"
+        />
+        <button type="submit" className="btn btn-block">
+          {isLoading ? "..." : "Submit"}
+        </button>
+        <p>
+          {values.isMember ? "Not a member yet?" : "Already a member?"}
+          <button
+            type="button"
+            onClick={toggleMember}
+            className="member-btn"
+            disabled={isLoading}
+          >
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
